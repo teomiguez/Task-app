1103189478e58ea6c56d29fd2379e173782ca0b2
teomiguez <teo.miguez215@gmail.com>
2023-07-16 18:37:26 -0300

    updateShema

--- Schema/tasks_db.sql
+++ Schema/tasks_db.sql
@@ -1,10 +1,36 @@
-CREATE DATABASE tasks_db;
+--
+-- Script base de datos `chat_db`
+--
 
+CREATE DATABASE tasks_db;
 USE tasks_db;
 
+/*----------------------------------------------------------*/
+
+--
+-- Estructura de tabla para la tabla `tasks`
+--
+
 CREATE TABLE tasks (
-    id INT(11) NOT NULL AUTO_INCREMENT,
+    id INT(11) NOT NULL,
     name VARCHAR(50) NOT NULL ,
-    description VARCHAR(200) NOT NULL,
-    CONSTRAINT pk_taskId PRIMARY KEY (id)
+    description VARCHAR(200) NOT NULL
 );
+
+/*----------------------------------------------------------*/
+/*----------------------------------------------------------*/
+
+--
+-- Indices de la tabla `tasks`
+--
+ALTER TABLE tasks
+  ADD PRIMARY KEY (id);
+
+/*----------------------------------------------------------*/
+/*----------------------------------------------------------*/
+
+--
+-- AUTO_INCREMENT de la tabla `tasks`
+--
+ALTER TABLE tasks
+  MODIFY id int(11) NOT NULL AUTO_INCREMENT;
